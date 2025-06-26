43c130814878eeebcf3779b2131b4057195e13ab
Luis Fyllypy <luisfyllypy@hotmail.com>
2025-06-26 06:53:08 -0300

    gunicorn

--- condoflow/settings.py
+++ condoflow/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qtv-$*-sie8ur348mlv@#!_uxao@4!uoj5-x=*lk)c=^q=(xn3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
